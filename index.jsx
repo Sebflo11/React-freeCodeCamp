e1ae57313467d29e4b2f8ffc0a971e1474970fe6
Sebastián Flores <sebflodev@gmail.com>
2023-09-14 21:29:58 -0300

    Commit inicial

---
+++ index.jsx
@@ -0,0 +1,28 @@
+const JSX = <div>
+    <h1></h1>
+    <p></p>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</div>
+
+// Agrgar comentarios en JSX
+
+const JSX = (
+    <div>
+        {/*  */}
+      <h1>This is a block of JSX</h1>
+      <p>Here's a subtitle</p>
+    </div>
+);
+
+// Renderiza elementos HTML al DOM
+const JSX = (
+    <div>
+      <h1>Hello World</h1>
+      <p>Lets render this to the DOM</p>
+    </div>
+);
+// Agrega tu código debajo de esta línea
