d8df3768891315876728812c596d8c114c96adc0
Sebastián Flores <sebflodev@gmail.com>
2023-09-14 22:08:58 -0300

    Seguimos estudiando

--- index.jsx
+++ index.jsx
@@ -67,8 +67,31 @@ class MyComponent extends React.Component {
                 <h1>Hello React!</h1>
             </div>
         );
-  
-  
       // Cambia el código encima de esta línea
+    }
+};
+
+// Crear un componente con composición
+const ChildComponent = () => {
+    return (
+      <div>
+        <p>I am the child</p>
+      </div>
+    );
+  };
+  
+  class ParentComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>I am the parent</h1>
+          { /* Cambia el código debajo de esta línea */ }
+            <ChildComponent />
+          { /* Cambia el código encima de esta línea */ }
+        </div>
+      );
     }
 };
